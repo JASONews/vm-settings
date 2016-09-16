b26d07be8c7b84baff50b1faa40b2565924f652e
Xinwen Wang <wxwjason@gmail.com>
2016-09-16 19:02:41 -0400

    add install for pwl fonts

---
+++ neovim/pwl_install.sh
@@ -0,0 +1,10 @@
+if test -d fonts; then
+	git clone https://github.com/powerline/fonts.git
+	cd fonts &&  . install.sh  && cd - && echo "powerline fonts done"
+	rm -rf fonts
+else
+	echo "fonts directory already exists" && exit 1
+fi
+
+
+
