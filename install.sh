39a3c5c8311557261d801fcbe17759723e4ea58f
Xinwen Wang <wxwjason@gmail.com>
2016-09-16 18:20:56 -0400

    add install for neovim and tmux

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+if test "$OSTYPE" = "linux-gnu"; then
+	sudo apt-get update
+	sudo apt-get install tmux
+	sudo apt-get install neovim
+fi
 . ./neovim/install.sh && echo "neovim done"
 . ./tmux/install.sh && echo "tmux done"
 . ./bash/install.sh && echo "bash done"
