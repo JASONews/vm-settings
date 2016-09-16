b02a2ce854812221273baef47010a4fcc73bc14a
Xinwen Wang <wxwjason@gmail.com>
2016-09-16 18:13:44 -0400

    add bash configuration

---
+++ bash/install.sh
@@ -0,0 +1,2 @@
+cp ./bashrc ~/.bashrc && echo "bashrc done"
+test -f  ~/.inputrc || cp ./inputrc ~/.inputrc && echo "inputrc done"
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 . ./neovim/install.sh && echo "neovim done"
 . ./tmux/install.sh && echo "tmux done"
+. ./bash/install.sh && echo "bash done"
