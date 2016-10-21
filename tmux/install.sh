f099fe65bf4f93c80aac246aead3626caffbf002
Xinwen Wang <wxwjason@gmail.com>
2016-10-20 21:55:14 -0400

    fix tmux install

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,6 +1,6 @@
 which tmux 
 test  $? -eq 0 || (echo "Please install tmux first" && exit 1)
 cp ./.tmux.conf ~/
-test  $? -eq 0 && (echo "tmux.conf done" && return 0)
-cp ./.shell_tmux ~/ && (echo "shell_tmux done" && return 0)
+test  $? -eq 0 && (echo "tmux.conf done" && exit 0)
+cp ./.shell_tmux ~/ && (echo "shell_tmux done" && exit 0)
 echo "tmux failed"
